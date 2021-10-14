6f89d8e38e250676be4fe28b55147680623b5776
Rus <de.mr44@gmail.com>
2021-10-14 17:53:41 +0500

    Added the user's defaults interface

--- UserDefaultsExample/ViewController.swift
+++ UserDefaultsExample/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct KeyDefaults {
+    static let city = "city"
+    static let street = "street"
+    static let house = "house"
+    static let building = "building"
+    static let apartment = "apartment"
+}
 
+class ViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+
+    @IBOutlet weak var cityTextFIeld: UITextField!
+    @IBOutlet weak var streetTextField: UITextField!
+    @IBOutlet weak var houseTextField: UITextField!
+    @IBOutlet weak var buildingTextField: UITextField!
+    @IBOutlet weak var apartmentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        defaults.removeObject(forKey: KeyDefaults.city)
+//        defaults.removeObject(forKey: KeyDefaults.street)
+//        defaults.removeObject(forKey: KeyDefaults.house)
+//        defaults.removeObject(forKey: KeyDefaults.building)
+//        defaults.removeObject(forKey: KeyDefaults.apartment)
+        cityTextFIeld.text = defaults.string(forKey: KeyDefaults.city)
+        streetTextField.text = defaults.string(forKey: KeyDefaults.street)
+        houseTextField.text = defaults.string(forKey: KeyDefaults.house)
+        buildingTextField.text = defaults.string(forKey: KeyDefaults.building)
+        apartmentTextField.text = defaults.string(forKey: KeyDefaults.apartment)
     }
 
-
+    @IBAction func saveButton(_ sender: Any) {
+        
+        let city = cityTextFIeld.text!
+        let street = streetTextField.text!
+        let house = houseTextField.text!
+        let apartment = apartmentTextField.text!
+        let building = buildingTextField.text!
+        
+        if !city.isEmpty && !street.isEmpty && !house.isEmpty && !apartment.isEmpty {
+            defaults.setValue(city, forKey: KeyDefaults.city)
+            defaults.setValue(street, forKey: KeyDefaults.street)
+            defaults.setValue(house, forKey: KeyDefaults.house)
+            defaults.setValue(building, forKey: KeyDefaults.building)
+            defaults.setValue(apartment, forKey: KeyDefaults.apartment)
+        }
+    }
 }
 
