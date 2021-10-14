839859c1607fcb475f8314cc1172c1881582b104
Rus <de.mr44@gmail.com>
2021-10-14 23:07:39 +0500

    Added the addresses page

---
+++ UserDefaultsExample/AddressesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddressesViewController.swift
+//  UserDefaultsExample
+//
+//  Created by ruslan on 14.10.2021.
+//
+
+import UIKit
+
+class AddressesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension AddressesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
