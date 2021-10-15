1e16aa8cc7152f7c0ab120e467403730efb439ce
Rus <de.mr44@gmail.com>
2021-10-15 16:14:30 +0500

    Added Base class

--- UserDefaultsExample/AddressesViewController.swift
+++ UserDefaultsExample/AddressesViewController.swift
@@ -9,23 +9,28 @@ import UIKit
 
 class AddressesViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.dataSource = self
-        tableView.delegate = self
+        
     }
 }
 
 extension AddressesViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         return UITableViewCell()
     }
 }
---
+++ UserDefaultsExample/Base.swift
@@ -0,0 +1,45 @@
+//
+//  Base.swift
+//  UserDefaultsExample
+//
+//  Created by ruslan on 14.10.2021.
+//
+
+import Foundation
+
+class Base {
+    
+    static let shared = Base()
+    let defaults = UserDefaults.standard
+    
+    struct UserAddress: Codable {
+        var city: String
+        var street: String
+        var house: String
+        var building: String?
+        var apartment: String
+    }
+    
+    var addresses: [UserAddress] {
+        
+        get {
+            if let data = defaults.value(forKey: "addresses") as? Data {
+                return try! PropertyListDecoder().decode([UserAddress].self, from: data)
+            } else {
+                return [UserAddress]()
+            }
+        }
+        
+        set {
+            if let data = try? PropertyListEncoder().encode(newValue) {
+                defaults.set(data, forKey: "addresses")
+            }
+        }
+    }
+    
+    func saveAddress(city: String, street: String, house: String, building: String?, apartment: String) {
+        
+        let address = UserAddress(city: city, street: street, house: house, building: building, apartment: apartment)
+        addresses.insert(address, at: 0)
+    }
+}
